fb6c2988e058f0b7bdf8f5c3d570f900e1e84a85
BjornFJohansson <bjornjobb@gmail.com>
2021-09-06 20:00:04 +0100

    formatted code

--- webpcr.py
+++ webpcr.py
@@ -22,58 +22,60 @@ from Bio.SeqUtils import MeltingTemp as _mt
 
 from flask_wtf import FlaskForm
 
-nn_tableoptions = [(1, "DNA_NN1 - Breslauer et al. (1986), Proc Natl Acad Sci USA 83: 3746-3750"),
-                   (2, "DNA_NN2 - Sugimoto et al. (1996), Nuc Acids Res 24 : 4501-4505"),
-                   (3, "DNA_NN3 - Allawi and SantaLucia (1997), Biochemistry 36: 10581-10594"),
-                   (4, "DNA_NN4 - SantaLucia & Hicks (2004), Annu. Rev. Biophys. Biomol. Struct 33: 415-440"),]
+nn_tableoptions = [(1, "DNA_NN1 - Breslauer et al. (1986), "
+                       "Proc Natl Acad Sci USA 83: 3746-3750"),
+                   (2, "DNA_NN2 - Sugimoto et al. (1996),"
+                       " Nuc Acids Res 24 : 4501-4505"),
+                   (3, "DNA_NN3 - Allawi and SantaLucia (1997),"
+                       " Biochemistry 36: 10581-10594"),
+                   (4, "DNA_NN4 - SantaLucia & Hicks (2004),"
+                       " Annu. Rev. Biophys. Biomol. Struct 33: 415-440")]
 
 
-saltoptions = [(1, "16.6 x log[Na+] (Schildkraut & Lifson (1965), Biopolymers 3: 195-208)"),
-               (2, "16.6 x log([Na+]/(1.0 + 0.7*[Na+])) (Wetmur (1991), Crit Rev Biochem Mol Biol 126: 227-259)"),
-               (3, "12.5 x log(Na+] (SantaLucia et al. (1996), Biochemistry 35: 3555-3562"),
-               (4, "11.7 x log[Na+] (SantaLucia (1998), Proc Natl Acad Sci USA 95: 1460-1465"),
-               (5, "Correction for deltaS: 0.368 x (N-1) x ln[Na+] (SantaLucia (1998), Proc Natl Acad Sci USA 95: 1460-1465)"),
-               (6, "(4.29(%GC)-3.95)x1e-5 x ln[Na+] + 9.40e-6 x ln[Na+]^2 (Owczarzy et al. (2004), Biochemistry 43: 3537-3554)"),
-               (7, "Complex formula with decision tree and 7 empirical constants.")]
+saltoptions = [(1, "16.6 x log[Na+] (Schildkraut & Lifson"
+                   " (1965), Biopolymers 3: 195-208)"),
+               (2, "16.6 x log([Na+]/(1.0 + 0.7*[Na+])) (Wetmur"
+                   " (1991), Crit Rev Biochem Mol Biol 126: 227-259)"),
+               (3, "12.5 x log(Na+] (SantaLucia et al. (1996),"
+                   " Biochemistry 35: 3555-3562"),
+               (4, "11.7 x log[Na+] (SantaLucia (1998),"
+                   " Proc Natl Acad Sci USA 95: 1460-1465"),
+               (5, "Correction for deltaS: 0.368 x (N-1) x ln[Na+] (Santa"
+                   "Lucia (1998), Proc Natl Acad Sci USA 95: 1460-1465)"),
+               (6, "(4.29(%GC)-3.95)x1e-5 x ln[Na+] + 9.40e-6 x ln[Na+]^2"
+                   " (Owczarzy et al. (2004), Biochemistry 43: 3537-3554)"),
+               (7, "Complex formula with decision tree and"
+                   " 7 empirical constants.")]
 
 
 class CustomForm(FlaskForm):
     """docstring."""
+
     table = SelectField('nn_table',
-                       choices=nn_tableoptions,
-                       default=4)
+                        choices=nn_tableoptions,
+                        default=4)
     salt = SelectField('saltcorr',
                        choices=saltoptions,
                        default=7)
-    Na = DecimalField("Na", default = 40)
-    Mg = DecimalField("Mg", default = 1.5)
-    dnac1 = DecimalField("dnac1", default = 250)
-    dnac2 = DecimalField("dnac2", default = 250)
+    Na = DecimalField("Na", default=40)
+    Mg = DecimalField("Mg", default=1.5)
+    dnac1 = DecimalField("dnac1", default=250)
+    dnac2 = DecimalField("dnac2", default=250)
 
-    K = DecimalField("K", default = 0)
-    Tris = DecimalField("Tris", default = 75.0)
-    dNTPs = DecimalField("dNTPs", default = 0.8)
+    K = DecimalField("K", default=0)
+    Tris = DecimalField("Tris", default=75.0)
+    dNTPs = DecimalField("dNTPs", default=0.8)
 
-    primer_text = TextAreaField('primer_text', 
-                                default = ">MyPrimer\ngctactacacacgtactgactg")
+    primer_text = TextAreaField('primer_text',
+                                default=">MyPrimer\ngctactacacacgtactgactg")
     send = SubmitField('send')
     clear = SubmitField('clear')
 
 
-
-nn_tables = {"1":_mt.DNA_NN1,
-             "2":_mt.DNA_NN2, 
-             "3":_mt.DNA_NN3,
-             "4":_mt.DNA_NN4}
-
-# saltcorrs = {"1":,
-#              "2":, 
-#              "3":,
-#              "4":
-#              "5":, 
-#              "6":,
-#              "7":}
-
+nn_tables = {"1": _mt.DNA_NN1,
+             "2": _mt.DNA_NN2,
+             "3": _mt.DNA_NN3,
+             "4": _mt.DNA_NN4}
 
 app = Flask(__name__)
 
@@ -87,13 +89,13 @@ results = []
 
 separator = '-'*80
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     """docstring."""
     print(url_for('tm'))
     """docstring."""
     return render_template("index.html")
-
 
 
 @app.route("/tm", methods=["GET", "POST"])
@@ -103,36 +105,34 @@ def tm():
     if request.method == "GET":
         return render_template("tm.html",
                                form=form)
-    
-    #if request.method == 'POST' and form.validate():
 
     if 'clear' in request.form:
         return redirect(url_for('tm'))
 
     user_data = request.form
-    
+
     primers = parse(user_data['primer_text'])
-    
+
     comments = []
     for primer in primers:
-        tm = _mt.Tm_NN( primer.seq,
-                        check=True,
-                        strict=True,
-                        c_seq=None,
-                        shift=0,
-                        nn_table=nn_tables[user_data['table']],
-                        tmm_table=None,
-                        imm_table=None,
-                        de_table=None,
-                        dnac1=float(user_data['dnac1']), 
-                        dnac2=float(user_data['dnac2']),
-                        selfcomp=False,
-                        Na=float(user_data['Na']),
-                        K=float(user_data['K']),
-                        Tris=float(user_data['Tris']), 
-                        Mg=float(user_data['Mg']),  
-                        dNTPs=float(user_data['dNTPs']),  
-                        saltcorr=int(user_data['salt']))
+        tm = _mt.Tm_NN(primer.seq,
+                       check=True,
+                       strict=True,
+                       c_seq=None,
+                       shift=0,
+                       nn_table=nn_tables[user_data['table']],
+                       tmm_table=None,
+                       imm_table=None,
+                       de_table=None,
+                       dnac1=float(user_data['dnac1']),
+                       dnac2=float(user_data['dnac2']),
+                       selfcomp=False,
+                       Na=float(user_data['Na']),
+                       K=float(user_data['K']),
+                       Tris=float(user_data['Tris']),
+                       Mg=float(user_data['Mg']),
+                       dNTPs=float(user_data['dNTPs']),
+                       saltcorr=int(user_data['salt']))
         primer.description = f"tm={tm}"
         comments.append(primer.format("fasta"))
     return render_template("tm.html",
@@ -161,8 +161,8 @@ def tm():
     # dnac2= user_data['dnac2']
     # contents = user_data['contents']
     # run = user_data['run']
-    #form.process()
-    #form.validate_on_submit()
+    # form.process()
+    # form.validate_on_submit()
 
 
 # def tm_default(
@@ -188,13 +188,9 @@ def tm():
 # ):
 
 
-
-
-
 @app.route("/pcr", methods=["GET", "POST"])
 def pcr():
     """docstring."""
-
     if request.method == "GET":
         return render_template("pcr.html",
                                results=results,
