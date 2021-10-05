00fc6fcaf3c3b6942cb9d5f0e921eaf2778b531c
BjornFJohansson <bjornjobb@gmail.com>
2021-10-05 07:35:43 +0100

    fix build script

--- build_with_pyinstaller.sh
+++ build_with_pyinstaller.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pyinstaller -w -F --add-data "templates:templates" --add-data "static:static" webpcr_app.py
+pyinstaller --windowed --onefile --add-data "templates:templates" --add-data "static:static" webpcr_app.py
 
 echo "press any key to close"
 read -n1 slask
