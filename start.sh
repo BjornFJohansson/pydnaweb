4b9167a5595929b7aafce451fa7496d2d55cdfe7
BjornFJohansson <bjornjobb@gmail.com>
2023-09-28 16:24:56 +0100

    bugfix

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-eval "$(/home/bjorn/anaconda3/condabin/conda shell.bash hook)"
+# eval "$(/home/bjorn/mambaforge/condabin/mamba shell.bash hook)"
 
-conda activate flaskapp
+mamba activate bjorn311
 
 export FLASK_APP=pydnaweb.py&&export FLASK_ENV=development&&flask run
 
